c310b0b336e4de561c28d8653cb5f736827663f4
imsaeef <iamsaifulislam23@gmail.com>
2022-05-15 17:28:16 +0600

    problemSolving

---
+++ credCoins.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t,x,y;
+    cin >> t;
+    while(t--){
+        cin >> x >> y;
+        int z = x*y / 100;
+        cout << z << endl;
+    }
+} 

5fb8f747538f11dd9a3ae77c6f8914f503663f99
imsaeef <iamsaifulislam23@gmail.com>
2022-05-16 19:21:55 +0600

    problemSolving

---
+++ minCars.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+         int n, k;
+         cin >> n;
+         k = ceil((double)n/4);
+         cout << k << endl;
+    }
+}

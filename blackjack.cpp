5103bebb598413d91a48c3d2039f5033a1a10eca
imsaeef <iamsaifulislam23@gmail.com>
2022-05-25 22:12:32 +0600

    problemSolving

---
+++ blackjack.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b,c;
+	    cin >> a >> b;
+	    c = 21-a-b;
+	    if (1 <= c && c <= 10){
+	        cout << c << endl;
+	    }else{
+	        cout << -1 << endl;
+	    }
+	}
+	return 0;
+}
